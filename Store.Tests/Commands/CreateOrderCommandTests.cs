7924071b6a5e0911464b354e17aad03e55758ccb
Eduardo Felipe <edufslima9@gmail.com>
2022-08-16 18:15:59 -0300

    test: Testing commands

---
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,23 @@
+using Store.Domain.Commands;
+
+namespace Store.Tests.Commands
+{
+  [TestClass]
+  public class CreateOrderCommandTests
+  {
+    [TestMethod]
+    [TestCategory("Handlers")]
+    public void ShouldNotCreateOrderWhenInsertInvalidCommand()
+    {
+      var command = new CreateOrderCommand();
+      command.Customer = "";
+      command.ZipCode = "13411080";
+      command.PromoCode = "12345678";
+      command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+      command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+      command.Validate();
+
+      Assert.AreEqual(command.IsValid, false);
+    }
+  }
+}
