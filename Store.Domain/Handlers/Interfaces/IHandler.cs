6272328a75e36a06fb0498a624107865fed9aadf
Eduardo Felipe <edufslima9@gmail.com>
2022-08-16 19:03:51 -0300

    feat: Implementing Handlers

---
+++ Store.Domain/Handlers/Interfaces/IHandler.cs
@@ -0,0 +1,8 @@
+using Store.Domain.Commands.Interfaces;
+
+namespace Store.Domain.Handlers.Interfaces
+{
+  public interface IHandler<T> where T : ICommand {
+    ICommandResult Handle(T command);
+  }
+}
